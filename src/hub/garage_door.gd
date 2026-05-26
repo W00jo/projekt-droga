bebc1a8d90881735ba5592792e5676e19bc818dd
rzabka1 <jturovska@gmail.com>
2026-05-26 02:19:07 +0200

    Change Hub assets

--- src/hub/garage_door.gd
+++ src/hub/garage_door.gd
@@ -3,7 +3,3 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body is PlayerWASD:
 		GameManager.main_scene.exit_hub_and_start_run()
-		
-		#transition_animation.play("transition_fadeout")
-		#await transition_animation.animation_finished
-		#get_tree().change_scene_to_file.call_deferred("res://src/level/level.tscn")
