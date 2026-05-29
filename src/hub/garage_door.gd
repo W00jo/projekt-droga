b830d2572cf76ea3a5150f0136e2d1161690167a
rzabka1 <jturovska@gmail.com>
2026-05-29 17:22:58 +0200

    Add garage door animation

--- src/hub/garage_door.gd
+++ src/hub/garage_door.gd
@@ -1,5 +1,8 @@
 extends Area2D
+@export var door_animated_sprite:AnimatedSprite2D
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is PlayerWASD:
+		door_animated_sprite.play("door_open")
 		GameManager.main_scene.exit_hub_and_start_run()
